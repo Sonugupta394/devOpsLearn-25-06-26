79077bb603ff80dec2ed0a80b67e490b7adc2904
Sonu Gupta <sonugupta394@b18g95.online>
2026-06-26 14:12:08 +0530

    ew

---
+++ resourceGroup.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "rrg1"{
+name = "rg1"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg2"{
+name = "rg2"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg3"{
+name = "rg3"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg4"{
+name = "rg4"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg5"{
+name = "rg5"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg6"{
+name = "rg6"
+location = "westus"
+}
+
+resource "azurerm_resource_group" "rrg6"{
+name = "rg6"
+location = "westus"
+}
